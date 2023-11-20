5878971bb6f1f0fa6558fd4fa1996f954b77487b
Oleg Stepovoy <s.olegys0801@gmail.com>
2023-11-20 16:57:38 +0300

    fix unused import of router

--- backend/app/endpoints/api.py
+++ backend/app/endpoints/api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
 from app.endpoints.auth import router as auth_router
-from app.endpoints.items import router as cams_router
-from app.endpoints.files import router as files_router
+from app.endpoints.items import router as items_router
+# from app.endpoints.files import router as files_router
 
 
 router = APIRouter(
@@ -11,5 +11,5 @@ router = APIRouter(
 )
 
 router.include_router(auth_router)
-router.include_router(cams_router)
-router.include_router(files_router)
+router.include_router(items_router)
+# router.include_router(files_router)
